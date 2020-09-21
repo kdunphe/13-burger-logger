27784cd60045445089eda5f21afd980af28dfe27
kdunphe <57336712+kdunphe@users.noreply.github.com>
2020-09-20 20:17:37 -0400

    Database Setup

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE database burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+	id int AUTO_INCREMENT NOT NULL,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES("Cheddar Cheeseburger", true), ("Veggie Burger", true), ("Turkey Burger", true)
